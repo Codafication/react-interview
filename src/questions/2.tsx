316ddde69df65b772c4833f5f88df4501a65f375
NatchTheBitBucket <nsurana@elco-corp.com>
2025-03-21 12:52:28 +1100

    q2

--- src/questions/2.tsx
+++ src/questions/2.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Two = () => {
+  const [visible, setVisible] = useState(true);
+  const handleVisibility = () => {
+    setVisible(!visible)
+  };
+
   return (
     <div>
-      <button>Toggle</button>
-      <div>Toggle showing/hiding me</div>
+      <button onClick={handleVisibility}>Toggle</button>
+      {visible && <div>Toggle showing/hiding me</div>}
     </div>
   );
 };
